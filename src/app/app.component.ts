ffe924d8310bc1e2387deebdfe9b96dde8b49eb7
Dominic Meconi <domec@Dominics-MBP.attlocal.net>
2023-05-28 23:40:35 -0500

    navbar sticky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ConsoleLogger} from "@angular/compiler-cli";
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,27 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'website';
 }
+
+//Wait until the content of the page has been loaded prior to handling thi
+document.addEventListener("DOMContentLoaded", function() {
+  let navbar = document.getElementById("NavigationBar");
+  window.onscroll = function () {
+    stickyNav()
+  };
+
+  // !. is sure the value we want to access is not null
+  // ?. allows for null access and checking
+  function stickyNav(){
+    //Null check
+  if (navbar?.offsetHeight === null){
+    console.log("Offset found to be null")
+    return
+  }
+  if (window.scrollY >= navbar!.offsetTop){
+    console.log("Made it")
+    navbar!.classList.add("stickyNav")
+    }else {
+      navbar!.classList.remove("stickyNav")
+    }
+  }
+})
