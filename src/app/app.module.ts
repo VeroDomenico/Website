ecdf1f161c093e70334dc43091a4a67299339911
Dominic Meconi <domec@dominics-mbp.lan>
2023-06-05 01:23:55 -0500

    todo build contract form in resume section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { ResumePopupComponent } from './resume-popup/resume-popup.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSelectModule} from "@angular/material/select";
+import { MatInputModule} from "@angular/material/input";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     ResumePopupComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSelectModule,
+    MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
