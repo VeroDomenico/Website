23e7469b7e32da0fb751f86b367ed91bf7834ca5
Dominic Meconi <domec@dominics-mbp.lan>
2023-06-05 23:40:08 -0500

    attempt to pull data from firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatSelectModule} from "@angular/material/select";
 import { MatInputModule} from "@angular/material/input";
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
@@ -27,7 +30,9 @@ import { MatInputModule} from "@angular/material/input";
     MatFormFieldModule,
     MatIconModule,
     MatSelectModule,
-    MatInputModule
+    MatInputModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
 
   ],
   providers: [],
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -4,6 +4,8 @@ import { ResumePopupComponent } from '../resume-popup/resume-popup.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import {Firestore, collection, setDoc, doc, getDoc} from "@angular/fire/firestore";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-landing',
@@ -14,12 +16,32 @@ export class LandingComponent implements OnInit {
   messageList = ["Developer", "Designer", "Entrepreneur"];
   currentMessage = '';
   shouldContinue = true;
+  col:any;
 
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, db : Firestore) {
+  this.col = collection(db,'test');
+
+
+  }
+
+
+
+
 
   ngOnInit(): void {
+
     this.startTypewriter();
+
+    this.pullData()
   }
+  pullData(){
+    let docRef = doc(this.col,'test')
+    let docSnap = getDoc(docRef);
+
+    console.log("Document data:", docSnap);
+
+  }
+
   openResumeDialog() {
     const dialogRef = this.dialog.open(ResumePopupComponent,{
       panelClass: 'no-scroll-dialog'
